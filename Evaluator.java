e34e422f2747e2aa4ad6136be80daebd4d09f9f8
Arnav <arnieincyberland@gmail.com>
2023-09-09 18:57:27 -0700

    made Equation class

--- Evaluator.java
+++ Evaluator.java
@@ -2,4 +2,16 @@ public class Evaluator {
     public static void main(String[] args) {
         System.out.println("Hello, Monkey Butt!");
     }
+
+    public class Equation {
+        private int operand_1, operand_2;
+        private char operator;
+
+        public Equation(int operand_1, int operand_2, char operator) {
+            this.operand_1 = operand_1;
+            this.operand_2 = operand_2;
+            this.operator = operator;
+        }
+
+    }
 }
